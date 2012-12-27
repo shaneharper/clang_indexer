480410516a891c9189b09e54d3d85b0ab1310be1
Shane Harper <shane@shaneharper.net>
2012-12-26 23:18:26 -0800

    Minor clean-up of clic_update.sh.
    New index_filename function factors out common-code.
    Common-code now handles updating a database as well as creating a new database.

--- clic_update.sh
+++ clic_update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Specify files to index here
-SOURCE_PATH=`cd $1; pwd` # convert $1 to an absolute path
-find $SOURCE_PATH\
+SOURCE_PATH=$1
+SOURCE_ABSOLUTE_PATH=`cd $SOURCE_PATH; pwd`
+find $SOURCE_ABSOLUTE_PATH\
     -name "*.cpp" -or\
     -name "*.hpp" -or\
     -name "*.cxx" -or\
@@ -12,35 +12,33 @@ find $SOURCE_PATH\
     -name "*.h"\
     | sort > files2.txt
 
+index_filename() {
+    echo $1.i.gz | tr "/" "%"
+}
+
 add_to_index() {
-    INDEX_FILE=`echo ${1}.i.gz | tr "/" "%"`
     echo + $1
-    clic_add index.db $INDEX_FILE \
+    clic_add index.db $(index_filename $1) \
         `[ -r ${SOURCE_PATH}/.clang_complete ] && cat ${SOURCE_PATH}/.clang_complete` $1
 }
 
 remove_from_index() {
-    INDEX_FILE=`echo ${1}.i.gz | tr "/" "%"`
+    INDEX_FILENAME=$(index_filename $1)
     echo - $1
-    clic_rm index.db $INDEX_FILE
-    rm $INDEX_FILE
+    clic_rm index.db $INDEX_FILENAME
+    rm $INDEX_FILENAME
 }
 
 if [ ! -f index.db -o ! -f files.txt ]; then
     echo "Creating database"
     clic_clear index.db
-    for i in `cat files2.txt`; do
-        add_to_index $i
-    done
-    mv files2.txt files.txt
-    exit
+    > files.txt
+else
+    echo "Updating database"
 fi
 
-echo "Updating database"
-#Generate the list of files added since last time
-comm -23 files2.txt files.txt > filesadded.txt
-
-for i in `cat filesadded.txt`; do
+# Add added files
+for i in `comm -23 files2.txt files.txt`; do
     add_to_index $i
 done
 
@@ -57,5 +55,4 @@ for i in `cat files.txt`; do
     fi
 done
 
-rm filesadded.txt
 mv files2.txt files.txt
