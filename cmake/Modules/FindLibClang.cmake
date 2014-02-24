af41f70a05afb53dceff7327d7727a4149598416
Shane Harper <shane@shaneharper.net>
2014-02-24 11:19:49 +1100

    Update FindLibClang.cmake
    Based on https://github.com/Sarcasm/irony-mode/blob/develop/cmake/FindLibClang.cmake#L20
    
    The right thing to do would be to have a _common_ FindLibClang.cmake (e.g. /usr/share/cmake-2.8/Modules/FindLibClang.cmake) that can be used by anything that needs libclang.

--- cmake/Modules/FindLibClang.cmake
+++ cmake/Modules/FindLibClang.cmake
@@ -1,6 +1,50 @@
-find_path(LibClang_INCLUDE_DIR clang-c/Index.h)
+find_path(LibClang_INCLUDE_DIR clang-c/Index.h
+    HINTS ${LIBCLANG_INCLUDE_PATH}
+    PATHS
+    # LLVM Debian/Ubuntu nightly packages: http://llvm.org/apt/
+    /usr/lib/llvm-3.1/include/
+    /usr/lib/llvm-3.2/include/
+    /usr/lib/llvm-3.3/include/
+    /usr/lib/llvm-3.4/include/
+    /usr/lib/llvm-3.5/include/
+    # LLVM MacPorts
+    /opt/local/libexec/llvm-3.1/include
+    /opt/local/libexec/llvm-3.2/include
+    /opt/local/libexec/llvm-3.3/include
+    /opt/local/libexec/llvm-3.4/include
+    /opt/local/libexec/llvm-3.5/include
+    # LLVM Homebrew
+    /usr/local/Cellar/llvm/3.1/include
+    /usr/local/Cellar/llvm/3.2/include
+    /usr/local/Cellar/llvm/3.3/include
+    /usr/local/Cellar/llvm/3.4/include
+    /usr/local/Cellar/llvm/3.5/include
+    )
 
-find_library(LibClang_LIBRARY NAMES clang)
+find_library(LibClang_LIBRARY NAMES clang
+    HINTS ${LIBCLANG_LIBRARY_PATH}
+    PATHS
+    # LLVM Debian/Ubuntu nightly packages: http://llvm.org/apt/
+    /usr/lib/llvm-3.1/lib/
+    /usr/lib/llvm-3.2/lib/
+    /usr/lib/llvm-3.3/lib/
+    /usr/lib/llvm-3.4/lib/
+    /usr/lib/llvm-3.5/lib/
+    # LLVM MacPorts
+    /opt/local/libexec/llvm-3.1/lib
+    /opt/local/libexec/llvm-3.2/lib
+    /opt/local/libexec/llvm-3.3/lib
+    /opt/local/libexec/llvm-3.4/lib
+    /opt/local/libexec/llvm-3.5/lib
+    # LLVM Homebrew
+    /usr/local/Cellar/llvm/3.1/lib
+    /usr/local/Cellar/llvm/3.2/lib
+    /usr/local/Cellar/llvm/3.3/lib
+    /usr/local/Cellar/llvm/3.4/lib
+    /usr/local/Cellar/llvm/3.5/lib
+    # LLVM Fedora
+    /usr/lib/llvm
+    )
 
 set(LibClang_LIBRARIES ${LibClang_LIBRARY})
 set(LibClang_INCLUDE_DIRS ${LibClang_INCLUDE_DIR})
